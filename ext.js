7632bd8d54c12e99037154bbf045a443fdc8d232
thamesssa <thamesssa@outlook.com>
2023-04-07 22:22:23 +0200

    Hero Updatef

--- ext.js
+++ ext.js
@@ -1,4 +1,24 @@
+AOS.init({
+  offset: 200,
+  duration: 600,
+  easing: 'ease-in-sine',
+  delay: 100,
+});
+
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY >0);
 });
+
+
+
+
+     var typed = new Typed('.typed', {
+      strings: ["Developer.", "Designer.", "Full Stack Developer.","Web Developer", "Computing Student."],
+      typeSpeed: 80,
+      backSpeed: 70,
+      backDelay: 1500,
+      loop: true,
+      startDelay: 1000,
+      
+    });
